38903887ef9defbea98ffb972ca4dc427a1213c6
fuckuxdd <tihamer0831@gmail.com>
2024-10-07 11:02:34 +0200

    mindjakesz

--- miau.js
+++ miau.js
@@ -1,6 +1,11 @@
 const burgerMenu = document.querySelector(".fa-solid");
+
+
 const hiddenMenu = document.querySelector(".menu-section");
 
 burgerMenu.addEventListener("click", function () {
+
   hiddenMenu.classList.toggle("shown");
-});
+
+}
+ );
